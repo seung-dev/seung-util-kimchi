e0cb6db2eaeddc31ae8b9043cee9db3d974cc20e
seung <seung.dev@gmail.com>
2023-03-06 05:27:46 +0900

    feat: add username to SRequestHeader

--- src/main/java/seung/util/kimchi/types/SRequestHeader.java
+++ src/main/java/seung/util/kimchi/types/SRequestHeader.java
@@ -19,6 +19,9 @@ public class SRequestHeader extends SType {
 	private SLinkedHashMap header = new SLinkedHashMap();
 	
 	@Builder.Default
+	private SLinkedHashMap token = new SLinkedHashMap();
+	
+	@Builder.Default
 	private SLinkedHashMap cookie = new SLinkedHashMap();
 	
 	@Builder.Default
@@ -28,21 +31,57 @@ public class SRequestHeader extends SType {
 		this.header.add(key, value);
 	}
 	
+	public void header(SLinkedHashMap header) {
+		this.header.merge(header);
+	}
+	
 	public String header(String key) {
 		return this.header.get_text(key, "");
+	}
+	
+	public SLinkedHashMap header() {
+		return this.header;
+	}
+	
+	public void token(String key, Object value) {
+		this.token.add(key, value);
+	}
+	
+	public void token(SLinkedHashMap token) {
+		this.token.merge(token);
+	}
+	
+	public String token(String key) {
+		return this.token.get_text(key, "");
+	}
+	
+	public SLinkedHashMap token() {
+		return this.token;
 	}
 	
 	public void cookie(String key, Object value) {
 		this.cookie.add(key, value);
 	}
 	
+	public void cookie(SLinkedHashMap cookie) {
+		this.cookie.merge(cookie);
+	}
+	
 	public String cookie(String key) {
 		return this.cookie.get_text(key, "");
+	}
+	
+	public SLinkedHashMap cookie() {
+		return this.cookie;
 	}
 	
 	@SuppressWarnings("unchecked")
 	public void session(Object key, Object value) {
 		this.session.put(key, value);
+	}
+	
+	public void session(SLinkedHashMap session) {
+		this.session.merge(cookie);
 	}
 	
 	public Object session(Object key) {
@@ -51,6 +90,10 @@ public class SRequestHeader extends SType {
 	
 	public String session_text(Object key) {
 		return this.session.get_text(key, "");
+	}
+	
+	public SLinkedHashMap session() {
+		return this.session;
 	}
 	
 }
